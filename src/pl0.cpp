212cd3e96a8c916ac9887495838c8d02e63b0ca9
flumi3 <sebastian.flum@hotmail.de>
2021-01-21 12:27:42 +0100

    min requirements are implemented now i think

--- src/pl0.cpp
+++ src/pl0.cpp
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
 
 int yyerror(char *s) {
     fprintf(stderr, "%s\n", s);
@@ -7,7 +11,23 @@ int yyerror(char *s) {
 
 int yyparse();
 
-int main() {
-    yyparse();
-    return 0;  // wegen return value vorgaben noch auf anforderungsblatt schauen
+int main(int argc, char* argv[]) {
+    extern FILE *yyin;
+
+    // check whether the user added a file to compile
+    if (argc < 2) {
+        printf("Missing argument: File to compile");
+        return 1;
+    }
+
+    char *userInput= argv[1];  // file path input from user (without .pl0 extension)
+    char filePath[100];  // char array to hold the file path with the .pl0 extension
+    strcpy(filePath, userInput);  // copy the user input into filePath
+    strcat(filePath, ".pl0");  // add the .pl0 extension
+    
+    // open the file and pass it as input to the lexer 
+    yyin = fopen(filePath, "r");
+
+    int rc = yyparse();
+    return rc;
 }
