b2f4ac6cdf3205a836a2ec87e9b39df513dd9ec6
flumi3 <sebastian.flum@hotmail.de>
2021-01-21 11:43:48 +0100

    implemented main

--- src/pl0.cpp
+++ src/pl0.cpp
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
 
 int yyerror(char *s) {
     fprintf(stderr, "%s\n", s);
@@ -7,7 +11,19 @@ int yyerror(char *s) {
 
 int yyparse();
 
-int main() {
+int main(int argc, char* argv[]) {
+    extern FILE *yyin;
+
+    // check whether the user added a file to compile
+    if (argc < 2) {
+        printf("Missing argument: File to compile");
+        return 1;
+    }
+
+    // get passed filepath and open the file
+    char *filePath = argv[1];
+    yyin = fopen(filePath, "r");
+
     yyparse();
     return 0;  // wegen return value vorgaben noch auf anforderungsblatt schauen
 }
